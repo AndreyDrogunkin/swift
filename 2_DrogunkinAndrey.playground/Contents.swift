9403283f37f548f7c1026401a96fc2a41a4cc7d5
AndreyDrogunkin <andrey.drogunkin@gmail.com>
2019-02-04 09:27:55 +0300

    Homework lesson 2

---
+++ 2_DrogunkinAndrey.playground/Contents.swift
@@ -0,0 +1,36 @@
+//: A Cocoa based Playground to present user interface
+
+import Foundation
+
+/// 1. Функция, которая определяет, четное число или нечетные
+
+for x in 1...40 {
+    if x % 2 == 0 {
+        continue
+    }
+    print(x)
+    
+}
+
+/// 2. Функция, которая определяет, делится ли число без остатка на 3
+
+var x = [3,100,10,33,11,50]
+
+for element in x where x % 3 == 0 {
+    print (element)
+}
+
+// Создать возрастающий массив из 100 чисел
+
+for x in 1...100
+if x <= 100 {
+    continue
+    print(i)
+} if x > 100
+    break
+
+
+
+
+
+
